b56c82cf2b5f124fb7e2aad4735fc2c137b99f7d
Fabien <fabienallouis@gmail.com>
2013-10-21 18:50:36 +0100

    Adding private functions

--- mackerel.js
+++ mackerel.js
@@ -2,6 +2,15 @@ var Mackerel = (function(win, undefined){
   // Private properties
   var health = 100;
   var poisioned = false;
+  // Private functions
+  function reduceHealth(dmg){
+    health -= dmg;
+  }
+  function inflictPoison(){
+    if(poisioned) setTimeout(inflictPoison, 500); 
+    reduceHealth(1);
+  }
+  // Public properties and methods
   var Mackerel = {
     isFish: function(){
       return true;
